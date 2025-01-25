4da948201bd8e47f9d1227ebb92506816f856015
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2025-01-25 09:51:40 +0500

    commands added

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://final-hackathon-server.vercel.app";
 
 export default axios;
